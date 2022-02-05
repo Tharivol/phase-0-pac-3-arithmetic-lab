13f02c0f848a744f498d8cb7e75fa061698bc627
roberthughes <hughes.brewzz@gmail.com>
2022-02-05 15:59:36 -0700

    commit tree 3962a3aa3ac724787432b9f4aec7b92cb2db5030

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// let add = 5;
+// let subtract = 6;
+// let multiply = 10;
+// let divide = 12;
+
+
+let a = 2;
+let b = 3;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b) {
+    return a*b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+let n = 1
+function increment(n) {
+   return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+function makeInt(n)  {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
